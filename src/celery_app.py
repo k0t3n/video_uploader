f4f6f990dbac4ed99e679d5d0714fb588a01e36b
k0t3n <k0t3n.mail@gmail.com>
2021-03-22 01:57:28 +0300

    Fix `DJANGO_SETTINGS_MODULE`

--- src/celery_app.py
+++ src/celery_app.py
@@ -6,7 +6,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 app = Celery('celery-app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
